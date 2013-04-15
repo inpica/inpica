8dc67792817f8a44062c103b191bb2ad1f26451e
arianshams <shams.arian@gmail.com>
2013-04-15 14:44:23 -0700

    maintags

--- main/templatetags/maintags.py
+++ main/templatetags/maintags.py
@@ -15,3 +15,16 @@ def FurniturePicker(context, includePins):
 	else:
 		furnitures = None
 	return {"furnitures":furnitures, "user":context["user"],"STATIC_URL":context['STATIC_URL']}
+
+@register.inclusion_tag('snippet/furniture-dashboard.html',takes_context=True)
+def FurnitureDashboard(context, pageNumber):
+	furniture_list = m.Furniture.objects.filter(user=context['user']).order_by('-RCD')
+	paginator_furniture = Paginator(furniture_list, 5) 
+	page_furniture = pageNumber
+	try:
+		furniture = paginator_furniture.page(page_furniture)
+	except PageNotAnInteger:
+		furniture = paginator_furniture.page(1)
+	except EmptyPage:
+		furniture = paginator_furniture.page(paginator_furniture.num_pages)
+	return {'furniture':furniture, "STATIC_URL":context['STATIC_URL']}
